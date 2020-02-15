2ddd3d2f5ae376e44fa033be911bfaa276628bef
i-laird <ian.c.laird@hotmail.com>
2020-02-14 18:16:01 -0600

    setting up the project

--- src/main/java/Controller/CDController.java
+++ src/main/java/Controller/CDController.java
@@ -1,7 +1,23 @@
 package Controller;
 
+import Service.CDService;
+import edu.baylor.ecs.cloudhubs.prophetdto.communication.Communication;
+import edu.baylor.ecs.cloudhubs.prophetdto.communication.CommunicationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class CDController {
+
+     @Autowired
+     private CDService communicationService;
+
+     @GetMapping("generateDiagram")
+     public Communication createCommunicationDiagram(@RequestBody CommunicationRequest req){
+          return communicationService.createCommunicationDiagram(req);
+     }
 }
--- src/main/java/Service/CDService.java
+++ src/main/java/Service/CDService.java
@@ -1,7 +1,13 @@
 package Service;
 
+import edu.baylor.ecs.cloudhubs.prophetdto.communication.Communication;
+import edu.baylor.ecs.cloudhubs.prophetdto.communication.CommunicationRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CDService {
+
+    public Communication createCommunicationDiagram(CommunicationRequest req){
+        return null;
+    }
 }
