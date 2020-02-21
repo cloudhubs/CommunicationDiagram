d2fc44c8aebc4049c7a087d172e2b8066e8ca720
i-laird <ian.c.laird@hotmail.com>
2020-02-20 21:31:56 -0600

    working on the interpreter

--- src/main/java/CD/build/CDBuilder.java
+++ src/main/java/CD/build/CDBuilder.java
@@ -3,7 +3,11 @@ package CD.build;
 import CD.exception.BuildException;
 import CD.util.StringStack;
 import edu.baylor.ecs.cloudhubs.prophetdto.communication.Communication;
+import edu.baylor.ecs.cloudhubs.prophetdto.communication.Edge;
+import edu.baylor.ecs.cloudhubs.prophetdto.communication.Node;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 public class CDBuilder {
@@ -11,13 +15,51 @@ public class CDBuilder {
     private Stack<FunctionContext> callStack = new Stack<>();
     private StringStack sequenceNumber = new StringStack();
     private boolean ifconditionFlag = false;
-    private boolean startFunctionFlag = false;
+    private int startFunctionFlag = 0;
+    private boolean createVariableFlag = false;
     private String functionArguments = null;
+    private String functionObjectBeingCalled = null;
 
+    private Set<Node> nodes = new HashSet<>();
+    private Set<Edge> edges = new HashSet<>();
+
+    /**
+     * consumes a token
+     *
+     * token types:
+     *
+     * CREATE: indicates that an object is going to be created
+     *      params: objectType objectName
+     *
+     * START_IF: begins conditional execution of the following code
+     *      params: condition
+     *
+     * END_IF: ends last START_IF
+     *
+     * START_ELSE: starts block that will execute if the last IF did not execute
+     *
+     * END_ELSE: ends ELSE block
+     *
+     * START_FUNCTION: indicates that a function is going to be called
+     *  params: objectName functionName
+     *  NOTE: params token must be followed by another START_FUNCTION
+     *
+     *  Example:
+     *      START_FUNCTION bar foo START_FUNCTION
+     *
+     * END_FUNCTION: indicates that the function is ending
+     *
+     * @param token
+     */
     public void consume(AbstractToken token){
         FunctionContext currentContext = callStack.peek();
 
         switch(token.getType()){
+
+            // indicates that a variable is being declared
+            case CREATE:
+                createVariableFlag = true;
+                break;
 
             // if an if statement or an elif, the following stmt will be included as a condition
             case START_IF:
@@ -47,10 +89,13 @@ public class CDBuilder {
             case START_FUNCTION:
 
                 // if the function has already been initiated this begins the code for the next function
-                if(startFunctionFlag){
-                    startFunctionFlag = false;
+                if(startFunctionFlag > 0){
+                    startFunctionFlag = 0;
                     callStack.push(new FunctionContext());
                     // create an edge for this call !!!! TODO
+                }
+                else{
+                    startFunctionFlag = 1;
                 }
                 break;
             case END_FUNCTION:
@@ -60,16 +105,26 @@ public class CDBuilder {
 
             // if not one of the expected token
             case OTHER:
-                if(ifconditionFlag){
+                if(createVariableFlag){
+                    nodes.add(new Node(token.toString()));
+                }
+                else if(ifconditionFlag){
                     currentContext.getIfStack().push(token.toString());
                     ifconditionFlag = false;
                 }
-                else if(startFunctionFlag){
-                    functionArguments = token.toString();
-                    functionArguments = null;
+                else if(startFunctionFlag > 0){
+                    switch(startFunctionFlag){
+                        case 1:
+                            functionObjectBeingCalled = token.toString();
+                            break;
+                        case 2:
+                            functionArguments = token.toString();
+                    }
+                    ++ startFunctionFlag;
                 }
                 break;
         }
+        return;
     }
 
     public Communication build() throws BuildException {
